397b9127938273235ef2ecf356caa44ff89acb8c
wangdi <wangdi10216@163.com>
2026-07-24 10:41:18 +0800

    Docker status and github actions yml files

---
+++ src/main/java/com/didi/cicd/CiCdApplication.java
@@ -0,0 +1,13 @@
+package com.didi.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/didi/cicd/CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.didi.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
